0542930d062d56fc65d192f7c8a33c03c58a1c49
haiduc100 <ducnvgch190188@fpt.edu.vn>
2022-01-25 00:19:59 +0700

    new update

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Updated</p>
+        <p>Updated version 2</p>
       </header>
     </div>
   );
